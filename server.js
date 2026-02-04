0c3eebacb30dd07bf2767cb07f825ecc79663eeb
Your Name <jamesmartindumas4@gmail.com>
2026-02-04 13:20:56 +0800

    Test commit

---
+++ server.js
@@ -0,0 +1,30 @@
+//server js
+require ('dotenv').config();
+const express = require ('express');
+const app = express ();
+const connectDB = require('./src/config/db');
+
+connectDB();
+
+// Middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+//middleware
+app.use (express.json());
+
+//Load Config
+const PORT = process.env. PORT || 3000;
+const BASE_URI = process.env. BASE_URI || '/api/v1';
+
+const apiRoutes = require ('./src/routes/apiRoutes');
+app.use (BASE_URI, apiRoutes);
+
+app.listen (PORT, () => {
+    console.log ('Server running on port ${PORT}');
+    console.log (`Base URI: http://localhost:${PORT}${BASE_URI}`);
+});
+
+
+
+
---
+++ src/routes/apiRoutes.js
@@ -0,0 +1,28 @@
+const express = require('express' );
+const router = express. Router( );
+
+// Import the Controller
+const {
+    getAllDishes,
+    createDish,
+    getDishById,
+    updateDish,
+    deleteDish,
+} = require('../controllers/dishController');
+
+// 1. GET /dishes → Get all dishes (Show menu)
+router.get('/dishes', getAllDishes);
+
+// 2. POST /dishes → Create a new dish
+router.post('/dishes', createDish);
+
+// 3. GET /dishes/:id → Get a specific dish by ID
+router.get('/dishes/:id', getDishById);
+
+// 4. PUT /dishes/:id → Update a dish (e.g., change meat)
+router.put('/dishes/:id', updateDish);
+
+// 5. DELETE /dishes/:id → Delete a dish
+router.delete('/dishes/:id', deleteDish);
+
+module.exports = router;
